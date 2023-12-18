0f81fd724d61281f86001864a5f99565b5342f37
camillalo1 <camillalo@getacademy.no>
2023-12-18 23:20:16 +0100

    Added methods for writing christmas card and showing them

--- GA-Julekortbanken/Program.cs
+++ GA-Julekortbanken/Program.cs
@@ -1,17 +1,60 @@
-﻿namespace GA_Julekortbanken
+﻿using System.ComponentModel;
+
+namespace GA_Julekortbanken
 {
-    internal class Program
+    public class Program
     {
         public List<XmasCard> Cards {  get; set; }
-        XmasCard xmasCard = new XmasCard("", "", "");
         public Program()
         {
             Cards = new List<XmasCard>();
         }    
-
         static void Main()
         {
+            bool RunProgram = true;
+            XmasCard xmasCard = new XmasCard("", "", "");
 
+            Console.WriteLine();
+            Console.WriteLine(" *** JULEKORTBANKEN *** ");
+            Console.WriteLine("------------------------");
+            Console.WriteLine();
+
+            while (RunProgram)
+            {
+                Console.WriteLine("Hovedmeny:  ");
+                Console.WriteLine("1. Vis alle julekortene");
+                Console.WriteLine("2. Lag et julekort");
+                Console.WriteLine("3. Rediger et julekort");
+                Console.WriteLine("4. Slett et julekort");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine();
+
+                int userChoice = Console.ReadKey().KeyChar;
+
+                switch (userChoice)
+                {
+                    case '1':
+                        xmasCard.ShowCards();
+                        break;
+                    case '2':
+                        xmasCard.AddCard();
+                        break;
+                    case '3':
+                        Console.WriteLine(" Prøv igjen senere");
+                        break;
+                    case '4':
+                        Console.WriteLine(" Prøv igjen senere");
+                        break;
+                    case '5':
+                        RunProgram = false;
+                        Console.WriteLine(" Hadet fint!");
+                        break;
+                    default: 
+                        Console.WriteLine(" Velg et av tallene 1-5.");
+                        break;
+                }
+
+            }
         }
     }
 }
--- GA-Julekortbanken/XmasCard.cs
+++ GA-Julekortbanken/XmasCard.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace GA_Julekortbanken
 {
-    internal class XmasCard
+    public class XmasCard
     {
+        Program program = new Program();
         public string ToName {  get; set; }
         public string FromName { get; set; }
         public string Greeting { get; set; }
@@ -17,6 +18,53 @@ namespace GA_Julekortbanken
             ToName = toName;
             FromName = fromName;
             Greeting = greeting;
+        }
+
+
+        public void ShowCards()
+        {
+            int cardNumber = 1;
+
+            foreach (var card in program.Cards)
+            {
+                Console.WriteLine($"{cardNumber} Til: {card.ToName}");
+                Console.WriteLine($"  {card.Greeting}");
+                Console.WriteLine($"  Hilsen {card.FromName}");
+                Console.WriteLine();
+                cardNumber++;
+            }
+        }
+
+        public void AddCard()
+        {
+            Console.WriteLine("Hvem vil du sende julekortet til?");
+            var toName = Console.ReadLine();
+            Console.WriteLine("Hvem vil du sette som avsender?");
+            var fromName = Console.ReadLine();
+            Console.WriteLine("Hva vil du skrive i kortet?");
+            var greeting = Console.ReadLine();
+
+     
+            XmasCard newCard = new XmasCard(toName, fromName, greeting);
+            program.Cards.Add(newCard);
+
+            Console.WriteLine("Hurra! Du har laget et julekort!");
+            Console.WriteLine("Slik ser det nye kortet ditt ut:");
+            Console.WriteLine();
+            Console.WriteLine($"Til: {newCard.ToName}");
+            Console.WriteLine($"{newCard.Greeting}");
+            Console.WriteLine($"Hilsen {newCard.FromName}");
+            Console.WriteLine();
+        }
+
+        public void EditCard()
+        {
+
+        }
+
+        public void DeleteCard()
+        {
+
         }
     }
 }
